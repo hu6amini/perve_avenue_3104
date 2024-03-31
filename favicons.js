17eb00948aa59d5bd28cbcada1ffd53feb94fc19
hu6amini <89549621+hu6amini@users.noreply.github.com>
2024-03-31 11:04:06 +0300

    Update favicons.js

--- favicons.js
+++ favicons.js
@@ -1,44 +1,50 @@
-function updateFaviconsForLinks(elements) {
-    elements.forEach(element => {
-        if (!(element.closest(".spoiler .code_top a") || element.closest(".fancyborder a") || element.closest(".quote_top a") || element.querySelector("img"))) {
-            let img = document.createElement("img");
-            if (element.href.includes("youtu.be")) {
-                img.src = "https://www.google.com/s2/favicons?domain=youtube.com";
-            } else {
-                img.src = "https://www.google.com/s2/favicons?domain=" + element.href;
-            }
-            img.alt = "fav";
-            if (element.matches(".quote a, .tmsg a")) {
-                img.width = 14;
-                img.height = 14;
-            } else {
-                img.width = 16;
-                img.height = 16;
-            }
-            element.prepend(img);
+function waitForElementToAppear(selector, callback) {
+    const intervalId = setInterval(() => {
+        const element = document.querySelector(selector);
+        if (element) {
+            clearInterval(intervalId);
+            callback(element);
         }
-    });
+    }, 100);
 }
 
-function waitForElementToAppear(selector, callback) {
-    const fav = new MutationObserver(mutations => {
-        mutations.forEach(mutation => {
-            if (mutation.addedNodes.length) {
-                const newElements = document.querySelectorAll(selector);
-                callback(newElements);
+waitForElementToAppear(".color a, span.tmsg a", (links) => {
+    function updateFaviconsForLinks(links) {
+        links.forEach((link) => {
+            if (
+                !(
+                    link.closest(".spoiler .code_top a") ||
+                    link.closest(".fancyborder a") ||
+                    link.closest(".quote_top a") ||
+                    link.querySelector("img")
+                )
+            ) {
+                const img = document.createElement("img");
+                if (link.href.includes("youtu.be")) {
+                    img.src = "https://www.google.com/s2/favicons?domain=youtube.com";
+                } else {
+                    img.src = "https://www.google.com/s2/favicons?domain=" + link.href;
+                }
+                img.alt = "fav";
+                if (link.matches(".quote a, .tmsg a")) {
+                    img.width = 14;
+                    img.height = 14;
+                } else {
+                    img.width = 16;
+                    img.height = 16;
+                }
+                link.prepend(img);
             }
+        });
+    }
+
+    const favObserver = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            updateFaviconsForLinks(mutation.target.querySelectorAll(".color a, span.tmsg a"));
         });
     });
 
-    const body = document.querySelector("body");
-    fav.observe(body, { childList: true, subtree: true });
+    favObserver.observe(links.parentElement, { childList: true, subtree: true });
 
-    const initialElements = document.querySelectorAll(selector);
-    if (initialElements.length) {
-        callback(initialElements);
-    }
-}
-
-waitForElementToAppear(".color a, span.tmsg a", function(newElements) {
-    updateFaviconsForLinks(newElements);
+    updateFaviconsForLinks(links);
 });
