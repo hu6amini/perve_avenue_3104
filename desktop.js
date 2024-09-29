ea03f6eca61143075ae4675d459861d606f0777d
jr <89549621+hu6amini@users.noreply.github.com>
2024-09-29 21:17:46 +0300

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -1,1 +1,1 @@
-!function(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_1185/pa_scripts.js",e.defer=!0,document.head.appendChild(e)}();
+!function(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_1186/pa_scripts.js",e.defer=!0,document.head.appendChild(e)}();
