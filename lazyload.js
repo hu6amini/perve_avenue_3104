a8dc457a3fcdf9ba4473e403c73986e90e6e6060
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-01 15:37:03 +0200

    Update lazyload.js

--- lazyload.js
+++ lazyload.js
@@ -1,6 +1,1 @@
-(function() {
-  var script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2069@main/lazyloading.js';
-  script.async = true; // Adding async to make sure it doesn't block rendering
-  document.head.appendChild(script);
-})();
+(function(){var e=document.createElement("script");e.src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js",e.integrity="sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==",e.crossOrigin="anonymous",e.referrerPolicy="no-referrer",e.async=!0,document.head.appendChild(e);var t=document.createElement("script");t.src="https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2070@main/lazyload.js",t.crossOrigin="anonymous",t.async=!0,document.head.appendChild(t)})();
