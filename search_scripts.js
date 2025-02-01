78ab581e8e64af941b45d062bb02afe4ad62c566
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-01 14:48:42 +0200

    Update search_scripts.js

--- search_scripts.js
+++ search_scripts.js
@@ -1,1 +1,6 @@
-document.addEventListener("DOMContentLoaded",(function(){if("search"===document.body.id){var e=document.createElement("script");e.src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js",e.async=!0,document.body.appendChild(e);var n=document.createElement("script");n.src="https://cdnjs.cloudflare.com/ajax/libs/emojione/4.5.0/lib/js/emojione.min.js",n.async=!0,document.body.appendChild(n),e.onload=function(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2062@main/pa_scripts.js",e.defer=!0,document.body.appendChild(e)};var t=document.createElement("script");t.src="https://nb.forumfree.it/scripts/ace/slider.js",t.defer=!0,document.body.appendChild(t)}}));
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2066@main/lazyload.js';
+  script.async = true; // Adding async to make sure it doesn't block rendering
+  document.head.appendChild(script);
+})();
