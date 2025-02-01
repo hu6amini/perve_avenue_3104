a486a2a1cd7df130a9337702050dd3a50ff4585c
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-01 15:23:49 +0200

    Update lazyload.js

--- lazyload.js
+++ lazyload.js
@@ -1,6 +1,6 @@
 (function() {
   var script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2068@main/lazyload.js';
+  script.src = 'https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2069@main/lazyloading.js';
   script.async = true; // Adding async to make sure it doesn't block rendering
   document.head.appendChild(script);
 })();
