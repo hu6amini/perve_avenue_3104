40db7cd1d524a495b2ad1bb05e485b637f7de202
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-08 21:10:29 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,1 +1,33 @@
-function loadScript(n,e,l,a,i,s){return new Promise(((r,t)=>{var o=document.createElement("script");o.src=n,e&&(o.integrity=e),l&&(o.crossOrigin=l),a&&(o.referrerPolicy=a),i&&(o.async=i),o.onload=()=>{s&&s(),r()},o.onerror=()=>r(),document.head.appendChild(o)}))}Promise.all([loadScript("https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js","sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==","anonymous","no-referrer",!0),loadScript("https://cdnjs.cloudflare.com/ajax/libs/emojione/4.5.0/lib/js/emojione.min.js","sha512-E2Ai/A9+KcoBm0lvfnd5krbr7TWUigQGWTfcoMToNpfmCvQKkZdTbpwyIM4PFbCGMtSmMjE/DAXGjVXpWGdFaQ==","anonymous","no-referrer",!0),loadScript("https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js","sha512-b4rL1m5b76KrUhDkj2Vf14Y0l1NtbiNXwV+SzOzLGv6Tz1roJHa70yr8RmTUswrauu2Wgb/xBJPR8v80pQYKtQ==","anonymous","no-referrer",!0),loadScript("https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js","sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==","anonymous","no-referrer",!0)]).then((()=>new Promise((n=>{document.addEventListener("DOMContentLoaded",n,{once:!0})})))).then((()=>Promise.all([loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2220@main/ui_enhancements.js",null,"anonymous",null,!0),loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2220@main/avatars.js",null,"anonymous",null,!0),loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2220@main/media.js",null,"anonymous",null,!0),loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2220@main/formatting.js",null,"anonymous",null,!0),loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2220@main/tooltips.js",null,"anonymous",null,!0),loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2220@main/links.js",null,"anonymous",null,!0),loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2220@main/utilities.js",null,"anonymous",null,!0)]))).then((()=>loadScript("https://nb.forumfree.it/scripts/ace/slider.js",null,null,"async",!0))).catch((()=>{}));
+function loadScript(n, e, l, a, i, s, d) {
+    return new Promise((r, t) => {
+        var o = document.createElement("script");
+        o.src = n;
+        e && (o.integrity = e);
+        l && (o.crossOrigin = l);
+        a && (o.referrerPolicy = a);
+        i && (o.async = i);
+        d && (o.defer = d); // Add defer attribute if specified
+        o.onload = () => {
+            s && s(), r();
+        };
+        o.onerror = () => r();
+        document.head.appendChild(o);
+    });
+}
+
+Promise.all([
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js", "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==", "anonymous", "no-referrer", true),
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/emojione/4.5.0/lib/js/emojione.min.js", "sha512-E2Ai/A9+KcoBm0lvfnd5krbr7TWUigQGWTfcoMToNpfmCvQKkZdTbpwyIM4PFbCGMtSmMjE/DAXGjVXpWGdFaQ==", "anonymous", "no-referrer", true),
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js", "sha512-b4rL1m5b76KrUhDkj2Vf14Y0l1NtbiNXwV+SzOzLGv6Tz1roJHa70yr8RmTUswrauu2Wgb/xBJPR8v80pQYKtQ==", "anonymous", "no-referrer", true),
+    loadScript("https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js", "sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==", "anonymous", "no-referrer", true)
+]).then(() => new Promise(n => {
+    document.addEventListener("DOMContentLoaded", n, { once: true });
+})).then(() => Promise.all([
+    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2221@main/ui_enhancements.js", null, "anonymous", null, false, null, true), // Added defer
+    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2221@main/avatars.js", null, "anonymous", null, false, null, true), // Added defer
+    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2221@main/media.js", null, "anonymous", null, false, null, true), // Added defer
+    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2221@main/formatting.js", null, "anonymous", null, false, null, true), // Added defer
+    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2221@main/tooltips.js", null, "anonymous", null, false, null, true), // Added defer
+    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2221@main/links.js", null, "anonymous", null, false, null, true), // Added defer
+    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2221@main/utilities.js", null, "anonymous", null, false, null, true) // Added defer
+])).then(() => loadScript("https://nb.forumfree.it/scripts/ace/slider.js", null, null, "async", false, null, true)).catch(() => {});
