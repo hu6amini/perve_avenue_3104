491d7884e7da3dfae1e29ab9de20ff4a0d285355
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-08 22:39:31 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -18,7 +18,6 @@ function loadScript(n, e, l, a, i, s, d) {
 Promise.all([
     loadScript("https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js", "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==", "anonymous", "no-referrer", false, null, true), // defer
     loadScript("https://cdnjs.cloudflare.com/ajax/libs/emojione/4.5.0/lib/js/emojione.min.js", "sha512-E2Ai/A9+KcoBm0lvfnd5krbr7TWUigQGWTfcoMToNpfmCvQKkZdTbpwyIM4PFbCGMtSmMjE/DAXGjVXpWGdFaQ==", "anonymous", "no-referrer", false, null, true), // defer
-    loadScript("https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js", "sha512-b4rL1m5b76KrUhDkj2Vf14Y0l1NtbiNXwV+SzOzLGv6Tz1roJHa70yr8RmTUswrauu2Wgb/xBJPR8v80pQYKtQ==", "anonymous", "no-referrer", false, null, true), // defer
     loadScript("https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js", "sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==", "anonymous", "no-referrer", false, null, true) // defer
 ]).then(() => new Promise(n => {
     document.addEventListener("DOMContentLoaded", n, { once: true });
