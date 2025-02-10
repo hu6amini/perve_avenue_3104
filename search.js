ee30eb0256f80b1b9cf79ac9360df5ea4c42ec7f
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-10 23:19:20 +0200

    Update search.js

--- search.js
+++ search.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // Load Perve Avenue and ACE Slider last
         let paScript = document.createElement("script");
-        paScript.src = "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2276@main/pa_scripts.js";
+        paScript.src = "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2277@main/pa_scripts.js";
         paScript.defer = true;
         document.body.appendChild(paScript);
 
