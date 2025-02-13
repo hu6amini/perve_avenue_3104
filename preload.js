73eb6866a642fbdc6b2157c7f3fcdc9634910d2d
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-14 01:16:16 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -90,7 +90,7 @@ Promise.all([
 // **Load additional scripts after DOM is ready**
 .then(function() {
     return Promise.all([
-        loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2312@main/pa_scripts.js", 
+        loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue_2313@main/pa_scripts.js", 
                    null, "anonymous", null, false, true) // defer
     ]);
 })
