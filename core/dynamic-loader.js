7c6689418d5cf176c2273b150d63375a36213259
jr <89549621+hu6amini@users.noreply.github.com>
2026-07-04 20:44:03 +0300

    Update LIGHTGALLERY_CSS URL in dynamic-loader.js

--- core/dynamic-loader.js
+++ core/dynamic-loader.js
@@ -113,7 +113,7 @@ function injectStylesheet(url) {
 
 async function loadLightGallery() {
     const LIGHTGALLERY_CSS = [
-        "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@e3da0e57d21df428676468b7aa105d074d78e2f2/lightgallery@2.7.1/lightgallery.min.css",
+        "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@f83758f0c50b30afd3116074146d285069e9ee96/lightgallery@2.7.1/lightgallery.min.css",
         "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@e44a482dc929aec9979f410815e3bf7bdc233da7/lightgallery@2.7.1/lg-zoom.min.css",
         "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@c5a5f520f3985fb7ef4d90892360aba8bf55a2c0/lightgallery@2.7.1/lg-thumbnail.min.css",
         "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@b6a816af149a4736f9ee02135f35997b7c03eb4d/lightgallery@2.7.1/lg-fullscreen.min.css",
