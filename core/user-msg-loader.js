7222ae92989727606b4de9407706ad4c6ddbf010
jr <89549621+hu6amini@users.noreply.github.com>
2026-08-02 17:53:10 +0300

    Update boards.js script URL in user-msg-loader

--- core/user-msg-loader.js
+++ core/user-msg-loader.js
@@ -230,7 +230,7 @@ whenBodyReady(() => {
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@11588f18d3c85bfe9998aea25a0ad1412492c188/modules/media-dimensions.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@53196ca6691c60b1498a26676fda6590a4d01761/modules/breadcrumbs.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@a88198b93bbc0093b0d0d64be88d2e2472e79a89/modules/twemoji.min.js"),
-                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@cdfa2067a40805dadf39d99a2ba50dad028169de/modules/boards.js"),
+                    loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@02eadfec8f782e85fd60e1b6f853da50d453e20b/modules/boards.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@b0f473583279dba7a4c884fced43005a61726b69/modules/posts.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@403484e8351e4fd2b9f757b5c340979cf7d452b8/modules/modals.min.js"),
                     loadScript("https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@e9239f827e999f14a409e0734a91d7f87aecd84b/modules/messenger.js")
